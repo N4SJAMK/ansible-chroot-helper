710cab86e8b6f3a34dd12f5ea544391d81fb93a2
Janne Alatalo <janne.alatalo@jamk.fi>
2015-02-09 15:29:48 +0200

    Implement functions

--- jail.py
+++ jail.py
@@ -32,25 +32,30 @@ options:
         required: false
         default: null
 '''
+import os
+import os.path
+import shutil
 
 MEMORY_FILE = ''
 
-def is_file_present(path, root = '/'):
-    pass
+def is_file_present(root = '/'):
+    def _is_file_present(path):
+        return os.path.isfile(path)
+    return _is_file_present
 
 def get_arguments():
     pass
 
 def get_copy_to_jail_func(root_folder):
     def _copy_to_jail(path):
-        pass
+        shutil.copy2(path, os.path.join(root_folder, path))
     return _copy_to_jail
 
 def get_library_dependencies(command):
     pass
 
-def remove_file():
-    pass
+def remove_file(path):
+    os.remove(path)
 
 def get_old_files(memory_file):
     pass
@@ -86,10 +91,10 @@ def main():
 
         old_files = get_old_files(MEMORY_FILE)
 
-        reduntant_files = diff(managed_files, old_files)
+        reduntant_files = diff(old_files, managed_files)
 
         # Filter out the files that have already been copied
-        files = filter(is_file_present, managed_files)
+        files = filter(is_file_present(args['root_folder']), managed_files)
 
         map(copy_func, files)
         map(remove_file, reduntant_files)
