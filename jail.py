849d9c299abdcf7e5dcfc7e4c420c17c94a1586b
Janne Alatalo <janne.alatalo@jamk.fi>
2015-02-12 08:55:23 +0200

    Replace old jail.py with new improved jail-imp.py

--- jail.py
+++ jail.py
@@ -16,7 +16,7 @@ options:
             "present", "absent"
         ]
         aliases: []
-    jail_folder:
+    jail_dir:
         description:
             - chroot folder
         required: true
@@ -26,9 +26,14 @@ options:
             - what command should be available in jail
         required: false
         default: null
-    other:
+    other_files:
         description:
             - what else should be copied
+        required: false
+        default: null
+    dirs:
+        description:
+            - what other directories should be copied
         required: false
         default: null
 '''
@@ -41,47 +46,15 @@ import itertools
 
 MEMORY_FILE = '/var/ansible-jail.mem'
 
-def is_file(root = '/'):
-    def _is_file(path):
-        if root != '/':
-            path = resolve_jail_path(root)(path)
-        return os.path.isfile(path)
-    return _is_file
-
-def is_folder(root = '/'):
-    def _is_folder(path):
-        if root != '/':
-            path = resolve_jail_path(root)(path)
-        return os.path.isdir(path)
-    return _is_folder
-
+# Unpure functions
 def get_arguments(module):
     args = {}
     args['state'] = module.params['state']
-    args['jail_folder'] = module.params['jail_folder']
+    args['jail_dir'] = module.params['jail_dir']
     args['commands'] = module.params['commands'] or []
-    args['other'] = module.params['other'] or []
+    args['other_files'] = module.params['other_files'] or []
+    args['dirs'] = module.params['dirs'] or []
     return args
-
-def copy_file_to_jail(jail_folder):
-    def _copy_to_jail(path):
-        jail_path = resolve_jail_path(jail_folder)(path)
-        jail_dir_path = os.path.dirname(jail_path)
-        if not os.path.exists(jail_dir_path):
-            os.makedirs(jail_dir_path)
-        shutil.copy2(path, jail_path)
-
-    return _copy_to_jail
-
-def copy_folder_to_jail(jail_folder):
-    def _copy_folder_to_jail(path):
-        jail_path = resolve_jail_path(jail_folder)(path)
-        jail_dir_path = os.path.dirname(jail_path)
-        if not os.path.exists(jail_dir_path):
-            os.makedirs(jail_dir_path)
-        shutil.copytree(path, jail_path)
-
-    return _copy_folder_to_jail
 
 def get_library_dependencies(command):
     ldd_out = subprocess.check_output(['ldd', command])
@@ -93,30 +66,184 @@ def get_library_dependencies(command):
                 deps.append(match.group(1) or match.group(2))
     return deps
 
-def remove_file(file_path):
-    os.remove(file_path)
-
-def remove_folder(path):
-    shutil.rmtree(path)
-
-def get_old_files(memory_file):
+def get_managed_objects(memory_file):
     if not os.path.isfile(memory_file):
         return []
     with open(memory_file, 'r') as f:
         files = f.read().splitlines()
     return files
 
-def save_managed_files(files, memory_file):
+def get_jail_tree(jail_dir):
+    dir_tree = {}
+    # From http://code.activestate.com/recipes/577879-create-a-nested-dictionary-from-oswalk/
+    # -------------------------------------------------------------------------
+    dir = {}
+    jail_dir = jail_dir.rstrip(os.sep)
+    start = jail_dir.rfind(os.sep) + 1
+    for path, dirs, files in os.walk(jail_dir):
+        folders = path[start:].split(os.sep)
+        subdir = dict.fromkeys(files)
+        parent = reduce(dict.get, folders[:-1], dir)
+        parent[folders[-1]] = subdir
+    return dir
+    # -------------------------------------------------------------------------
+
+def destroy_jail(jail_dir, memory_file):
+    msg = {}
+    msg['changed'] = False
+    msg['msg'] = []
+    if os.path.isdir(jail_dir):
+        shutil.rmtree(jail_dir)
+        msg['changed'] = True
+        msg['msg'].append("rm {0}".format(jail_dir))
+    if os.path.isfile(memory_file):
+        remove_file(memory_file)
+        msg['changed'] = True
+        msg['msg'].append("rm {0}".format(memory_file))
+    return msg
+
+def take_actions(actions):
+    def _take_action(action):
+        action[0]()
+        return action[1]
+    return reduce(create_msg, map(_take_action, actions), {'changed': False, 'msg': []})
+
+def fake_actions(actions):
+    def _fake_action(action):
+        return action[1]
+    return reduce(create_msg, map(_fake_action, actions), {'changed': False, 'msg': []})
+
+def create_msg(msgs, msg):
+    msgs['changed'] = True
+    msgs['msg'].append(msg)
+    return msgs
+
+def save_managed_files(memory_file, files):
     with open(memory_file, 'w') as f:
         f.writelines(["{0}\n".format(x) for x in files if x != ''])
+
+# Pure functions
+# -----------------------------------------------------------------------------
+
+def create_actions(jail_dir, files, dirs, managed_objects, jail_tree, memory_file):
+
+    reduntant_objects = diff(managed_objects, files + dirs)
+    reduntant_files = itertools.ifilter(is_file(jail_tree), reduntant_objects)
+    reduntant_dirs = itertools.ifilter(is_dir(jail_tree), reduntant_objects)
+
+    rm_file_actions = map(create_rm_file_action(jail_dir), reduntant_files)
+    rm_dir_actions = map(create_rm_dir_action(jail_dir), reduntant_dirs)
+    rm_actions = rm_file_actions + rm_dir_actions
+
+    _missing_files = [x for x in itertools.ifilterfalse(is_file(jail_tree), files)]
+    missing_files = remove_duplicates(_missing_files)
+    missing_dirs = [x for x in itertools.ifilterfalse(is_dir(jail_tree), dirs)]
+
+    _parent_dirs = map(get_parent_dir_path, missing_files + missing_dirs)
+    parent_dirs = remove_duplicates(_parent_dirs)
+    missing_parent_dirs = itertools.ifilterfalse(is_dir(jail_tree), parent_dirs)
+
+    path_actions = map(create_make_path_action(jail_dir), missing_parent_dirs)
+    file_actions = map(create_cp_file_action(jail_dir), missing_files)
+    dir_actions = map(create_cp_dir_action(jail_dir), missing_dirs)
+
+    if len(missing_dirs) + len(missing_files) + len(reduntant_objects) != 0:
+        memory_file_action = [create_memory_file_action(memory_file, files + dirs)]
+    else:
+        memory_file_action = []
+
+    return rm_actions + path_actions + file_actions + dir_actions + memory_file_action
+
+def is_file(jail_tree):
+    def _is_file(file_path):
+        def walker(tree, file_path):
+            node = tree.get(file_path[0])
+            if len(file_path) == 1:
+                if file_path[0] in tree and node == None:
+                    return True
+                else:
+                    return False
+            elif node == None:
+                return False
+            else:
+                return walker(node, file_path[1:])
+        if jail_tree == {}:
+            return False
+        else:
+            return walker(jail_tree.values()[0] or {}, file_path.split("/")[1:])
+    return _is_file
+
+def is_dir(jail_tree):
+    def _is_dir(dir_path):
+        def walker(tree, dir_path):
+            node = tree.get(dir_path[0])
+            if node == None:
+                return False
+            if len(dir_path) == 1:
+                return True
+            else:
+                return walker(node, dir_path[1:])
+        if jail_tree == {}:
+            return False
+        else:
+            return walker(jail_tree.values()[0] or {}, dir_path.split("/")[1:])
+    return _is_dir
 
 def diff(a, b):
     return [x for x in a if x not in b]
 
-def resolve_jail_path(jail_folder):
-    def _resolve_jail_path(file_path):
-        return os.path.join(jail_folder, file_path[1:])
-    return _resolve_jail_path
+def resolve_jail_path(jail_dir, file_path):
+    return os.path.join(jail_dir, file_path[1:])
+
+def get_parent_dir_path(path):
+    return "/".join(path.split("/")[:-1])
+
+def remove_duplicates(l):
+    return list(set(l))
+
+def create_rm_file_action(jail_dir):
+    def _create_rm_file_action(f):
+        def _rm_file_action():
+            os.remove(resolve_jail_path(jail_dir, f))
+        return (_rm_file_action, "rm {0}".format(f))
+    return _create_rm_file_action
+
+def create_rm_dir_action(jail_dir):
+    def _create_rm_dir_action(d):
+        def _rm_dir_action():
+            shutil.rmtree(resolve_jail_path(jail_dir, d))
+        return (_rm_dir_action, "rm {0}".format(d))
+    return _create_rm_dir_action
+
+def create_cp_file_action(jail_dir):
+    def _create_cp_file_action(f):
+        file_jail_path = resolve_jail_path(jail_dir, f)
+        def _cp_file_action():
+            shutil.copy2(f, file_jail_path)
+        return (_cp_file_action, "cp {0} {1}".format(f, file_jail_path))
+    return _create_cp_file_action
+
+def create_cp_dir_action(jail_dir):
+    def _create_cp_dir_action(d):
+        dir_jail_path = resolve_jail_path(jail_dir, d)
+        def _cp_dir_action():
+            shutil.copytree(d, dir_jail_path)
+        return (_cp_dir_action, "cp {0} {1}".format(d, dir_jail_path))
+    return _create_cp_dir_action
+
+def create_make_path_action(jail_dir):
+    def _create_make_path_action(path):
+        def _make_path_action():
+            os.makedirs(resolve_jail_path(jail_dir, path))
+        return (_make_path_action, "mkdir {0}".format(path))
+    return _create_make_path_action
+
+def create_memory_file_action(memory_file, files):
+    def _memory_file_action():
+        save_managed_files(memory_file, files)
+    return (_memory_file_action, "save memory file to {0}".format(memory_file))
+
+# -----------------------------------------------------------------------------
 
 def main():
     arguments = {
@@ -125,45 +252,37 @@ def main():
             'choises'   : ['present', 'absent'],
             'default'   : 'present',
         },
-        'jail_folder'   : { 'required': True, 'default': None },
+        'jail_dir'      : { 'required': True, 'default': None },
+        'dirs'          : { 'default': None },
         'commands'      : { 'default': None },
-        'other'         : { 'default': None },
+        'other_files'   : { 'default': None },
     }
-    module = AnsibleModule(argument_spec = arguments)
+    module = AnsibleModule(argument_spec = arguments, supports_check_mode = True)
     args = get_arguments(module)
 
     if args['state'] == 'present':
-        # Get all library dependencies that all the commands have
+
+        # Collect data
         libs = sum(map(get_library_dependencies, args['commands']), [])
+        files = libs + args['commands'] + args['other_files']
+        dirs = args['dirs']
+        managed_objects = get_managed_objects(MEMORY_FILE)
+        jail_tree = get_jail_tree(args['jail_dir'])
 
-        managed_files = libs + args['commands'] + args['other']
+        # Pure function that returns unpure actions
+        actions = create_actions(args['jail_dir'], files, dirs, managed_objects, jail_tree, MEMORY_FILE)
 
-        #module.exit_json(msg = managed_files)
-
-        old_files = get_old_files(MEMORY_FILE)
-
-        _resolve_jail_path = resolve_jail_path(args['jail_folder'])
-
-        reduntant_files = map(_resolve_jail_path, diff(old_files, managed_files))
-
-        # Filter out the files and folders that have already been copied
-        files = itertools.ifilterfalse(is_file(args['jail_folder']), itertools.ifilter(is_file('/'), managed_files))
-        folders = itertools.ifilterfalse(is_folder(args['jail_folder']), itertools.ifilter(is_folder('/'), managed_files))
-
-        map(copy_file_to_jail(args['jail_folder']), files)
-        map(copy_folder_to_jail(args['jail_folder']), folders)
-        map(remove_file, itertools.ifilter(is_file(), reduntant_files))
-        map(remove_folder, itertools.ifilter(is_folder(), reduntant_files))
-
-        save_managed_files(managed_files, MEMORY_FILE)
+        # Take actions
+        if not module.check_mode:
+            msg = take_actions(actions)
+        else:
+            msg = fake_actions(actions)
+        
+        module.exit_json(**msg)
 
     else:
-        if os.path.isdir(args['jail_folder']):
-            shutil.rmtree(args['jail_folder'])
-        if os.path.isfile(MEMORY_FILE):
-            remove_file(MEMORY_FILE)
-
-    module.exit_json(changed = True, msg = "SUCCESS")
+        msg = destoy_jail(args['jail_dir'], MEMORY_FILE)
+        module.exit_json(msg)
 
 from ansible.module_utils.basic import *
 main()
